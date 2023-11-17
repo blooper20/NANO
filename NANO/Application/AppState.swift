58719d81acc131bdb729d0dab434288158378068
blooper20 <yjw127588@gmail.com>
2023-11-17 17:28:25 +0900

    MySongsViewModel 완성 및 AppState 생성

---
+++ NANO/Application/AppState.swift
@@ -0,0 +1,8 @@
+//
+//  AppState.swift
+//  NANO
+//
+//  Created by DwaeWoo on 2023/11/17.
+//
+
+import Foundation
---
+++ NANO/Presentation/ViewModels/MySongsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MySongsViewModel.swift
+//  NANO
+//
+//  Created by DwaeWoo on 2023/11/17.
+//
+
+import Foundation
